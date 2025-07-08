c6be9c5faf766aa8a8b875aa9ddcde456c9759de
aidil <yojesjasteb@gmail.com>
2025-07-08 10:07:33 -1200

    :white_check_mark: update feature login

--- frontend/src/components/elements/Input.jsx
+++ frontend/src/components/elements/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Input = React.memo(({label, placeholder}) => {
+const Input = React.memo(({label, placeholder, value, onChange, type = 'text'}) => {
   return (
     <div className='w-full flex flex-col gap-[7px]'>
       <label htmlFor="" className='w-full font-(--font-weight-1) text-(length:--size-text-1) '><p>{label}</p></label>
-      <input type="text" placeholder={placeholder} className='font-(--font-weight-2) text-(length:--size-text-2) w-full border-[1px] rounded-(--border-radius) border-(--border-color) px-[20px] py-[10px] '/>
+      <input type={type} placeholder={placeholder} value={value} onChange={onChange} className='font-(--font-weight-2) text-(length:--size-text-2) w-full border-[1px] rounded-(--border-radius) border-(--border-color) px-[20px] py-[10px] '/>
     </div>
   )
 })
--- frontend/src/utils/api/api.js
+++ frontend/src/utils/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { CurrentToken } from "../helper/TokenService";
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://localhost:8080/api/v1",
     headers: {
         Accept: "application/json",
         Authorization:`Bearer ${CurrentToken.get()}` 
