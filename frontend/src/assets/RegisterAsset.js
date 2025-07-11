6544d439c05f749b76438782f18871cdcd544e54
Galihuyy <gashana1321@gmail.com>
2025-07-11 14:22:16 +0700

    register all assets

--- frontend/src/assets/RegisterAsset.js
+++ frontend/src/assets/RegisterAsset.js
@@ -1,3 +1,12 @@
+import arrow_down from '../assets/icon/arrow_down.svg'
+import add from '../assets/icon/add.svg'
+import deleteIcon from '../assets/icon/delete.svg' 
+import edit from '../assets/icon/edit.svg'
+import logo_chrome from '../assets/icon/logo-chrome.png'
 import logo from '../assets/icon/logo.png'
+import pelanggan from '../assets/icon/pelanggan.svg'
+import pembayaran from '../assets/icon/pembayaran.svg'
+import search from '../assets/icon/search.svg'
+import transaksi from '../assets/icon/transaksi.svg'
 
-export {logo}
+export {logo, arrow_down, add, deleteIcon, edit, logo_chrome, pelanggan, pembayaran, search, transaksi}
