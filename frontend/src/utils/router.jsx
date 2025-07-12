2784d0fc2138e7a1fbf35dafef0f39ce41647245
Galihuyy <gashana1321@gmail.com>
2025-07-12 09:23:52 +0700

    add router yang diperlukan

--- frontend/src/utils/router.jsx
+++ frontend/src/utils/router.jsx
@@ -4,12 +4,12 @@ import GuestMiddleware from "./middleware/GuestMiddleware";
 import Login from "../pages/Authentication/login.jsx";
 import ForgetPassword from "../pages/Authentication/forget_password/ForgetPassword.jsx";
 import { ChangePassword } from "../pages/Authentication/forget_password/ChangePassword.jsx";
+import Pelanggan from "../pages/Client_area/Pelanggan/Index.jsx";
+import Dashboard from "../pages/Client_area/Dashboard/Index.jsx";
+import Invoice from "../pages/Invoice/Index.jsx";
+import Transaksi from "../pages/Client_area/Transaksi/Index.jsx";
 
 const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <AuthMiddleware> <h1>tess midd</h1> </AuthMiddleware>
-    },
     {
         path : '/login',
         element : <GuestMiddleware> <Login/></GuestMiddleware>
@@ -21,6 +21,22 @@ const router = createBrowserRouter([
     {
         path : '/forget-password/change-password',
         element : <GuestMiddleware> <ChangePassword/></GuestMiddleware>
+    },
+    {
+        path : '/',
+        element : <AuthMiddleware> <Dashboard/> </AuthMiddleware>
+    },
+    {
+        path : '/pelanggan',
+        element : <AuthMiddleware> <Pelanggan/> </AuthMiddleware>
+    },
+    {
+        path : '/transaksi',
+        element : <AuthMiddleware> <Transaksi/> </AuthMiddleware>
+    },
+    {
+        path : '/invoice',
+        element : <AuthMiddleware> <Invoice/> </AuthMiddleware>
     },
 ])
 
