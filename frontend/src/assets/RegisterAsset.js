e97018d0026853eeb17247130e1aa2bcb6dafeb5
Galihuyy <gashana1321@gmail.com>
2025-07-12 09:23:29 +0700

    registered icon

--- frontend/src/assets/RegisterAsset.js
+++ frontend/src/assets/RegisterAsset.js
@@ -4,9 +4,21 @@ import deleteIcon from '../assets/icon/delete.svg'
 import edit from '../assets/icon/edit.svg'
 import logo_chrome from '../assets/icon/logo-chrome.png'
 import logo from '../assets/icon/logo.png'
-import pelanggan from '../assets/icon/pelanggan.svg'
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
 import pembayaran from '../assets/icon/pembayaran.svg'
 import search from '../assets/icon/search.svg'
-import transaksi from '../assets/icon/transaksi.svg'
+import Home from '@mui/icons-material/HomeOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 
-export {logo, arrow_down, add, deleteIcon, edit, logo_chrome, pelanggan, pembayaran, search, transaksi}
+
+export {
+    logo, arrow_down, add, 
+    deleteIcon, edit, logo_chrome, 
+    PeopleOutlinedIcon, pembayaran, 
+    search, Home, ReceiptOutlinedIcon,
+    ReceiptLongOutlinedIcon, SearchOutlinedIcon,
+    NotificationsOutlinedIcon
+}
