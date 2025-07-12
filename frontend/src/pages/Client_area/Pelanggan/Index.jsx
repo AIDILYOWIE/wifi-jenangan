f3a85fa458fce4134a5f9fd397cb3db88c846fe5
Galihuyy <gashana1321@gmail.com>
2025-07-12 09:26:01 +0700

    add struktur page and use baselayout

---
+++ frontend/src/pages/Client_area/Pelanggan/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BaseLayout from '../../../components/layouts/BaseLayout'
+
+const Pelanggan = () => {
+  return (
+    <BaseLayout>
+      <h1>Pelanggan</h1>
+    </BaseLayout>
+  )
+}
+
+export default Pelanggan
---
+++ frontend/src/pages/Invoice/Index.jsx
@@ -0,0 +1,11 @@
+import BaseLayout from "../../components/layouts/BaseLayout"
+
+const Invoice = () => {
+  return (
+    <BaseLayout>
+      <h1>Invoice</h1>
+    </BaseLayout>
+  )
+}
+
+export default Invoice
