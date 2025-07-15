a74134f8bf4ca3204491ecdcad705ed55cca64c1
Galihuyyy <galihputrapratama.dev@gmail.com>
2025-07-15 20:01:24 +0700

    add get tagihan and confirm tagihan

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Authentication\AuthenticationController;
 use App\Http\Controllers\Paket\PaketController;
 use App\Http\Controllers\Pelanggan\PelangganController;
+use App\Http\Controllers\Transaksi\TagihanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,9 @@ Route::prefix('/v1')->group( function() {
     Route::post('/login', [AuthenticationController::class, 'login']);
     Route::resource('/pelanggan', PelangganController::class)->only(['index', 'store']);
     Route::resource('/pelanggan', PelangganController::class)->except(['index', 'store'])->middleware('validId:pelanggan');
+
+    Route::get('/tagihan', [TagihanController::class, 'index']);
+    Route::put('/tagihan/{tagihan}', [TagihanController::class, 'confirmTagihan'])->middleware('validId:tagihan');
 
     Route::resource('/paket', PaketController::class)->only(['index', 'store']);
     Route::resource('/paket', PaketController::class)->except(['index', 'store'])->middleware('validId:paket');
