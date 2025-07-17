3300855c9fd35c19032943222309eddf78438407
Galihuyyy <galihputrapratama.dev@gmail.com>
2025-07-17 10:31:17 +0700

    update open modal logic and add function get kode pelanggan

--- frontend/src/pages/Client_area/Pelanggan/Index.jsx
+++ frontend/src/pages/Client_area/Pelanggan/Index.jsx
@@ -1,16 +1,35 @@
 import BaseLayout from "../../../components/layouts/BaseLayout";
-import { PeopleAltOutlinedIcon } from "../../../assets/RegisterAsset";
-import Table from "../../../components/fragments/Table";
-import { useState } from "react";
+import { PeopleAltOutlinedIcon, PrintIcon } from "../../../assets/RegisterAsset";
+import Table from "./Datagrid/Table";
+import { useEffect, useState } from "react";
 import HeaderPage from "../../../components/fragments/Header";
 import AddPelanggan from "./Action/AddPelanggan";
+import { api } from "../../../utils/helper/helper";
 
 const Pelanggan = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [newCode, setNewCode] = useState('')
+
+  function toggleModalCreate() {
+    setOpen(!open)
+  }
+
+  const getNewCodePelanggan = async () => {
+    try {
+      const response = await api.get('/new-kode-pelanggan')
+      setNewCode(response.data.new_kode_pelanggan);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getNewCodePelanggan()
+  }, [])
   return (
     <BaseLayout text={"Pelanggan"}>
       <HeaderPage
-        onClick={() => setOpen(!open)}
+        onClick={() => toggleModalCreate()}
         icon={
           <PeopleAltOutlinedIcon
             sx={{
@@ -31,9 +50,9 @@ const Pelanggan = () => {
       </div>
 
       {/* Modal */}
-      <AddPelanggan open={open} setOpen={() => setOpen(!open)}/>
+      <AddPelanggan open={open} setOpen={() => setOpen(!open)} onClose={() => {toggleModalCreate()}} newCode={newCode}/>
     </BaseLayout>
   );
-};
+}; 
 
 export default Pelanggan;
