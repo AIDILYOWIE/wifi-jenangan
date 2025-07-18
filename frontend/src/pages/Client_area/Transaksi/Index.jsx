a08250428958a79b5eaa8ec9f0880923b3dc96df
Galihuyyy <galihputrapratama.dev@gmail.com>
2025-07-18 16:27:23 +0700

    update page transaksi

--- frontend/src/pages/Client_area/Transaksi/Index.jsx
+++ frontend/src/pages/Client_area/Transaksi/Index.jsx
@@ -1,11 +1,34 @@
+import { ReceiptOutlinedIcon } from "../../../assets/RegisterAsset"
+import HeaderPage from "../../../components/fragments/Header"
 import BaseLayout from "../../../components/layouts/BaseLayout"
+import Table from "./Datagrid/Table"
 
-const Transaksi = () => {
+const Invoice = () => {
   return (
-    <BaseLayout>
-      <h1>Transaksi</h1>
+    <BaseLayout >
+      <HeaderPage
+        text={"Transaksi"}
+        canCreate = {false}
+        icon={
+          <ReceiptOutlinedIcon
+            sx={{
+              fontSize: {
+                sm: "28px",
+                md: "38px",
+              },
+              color: "#19297C",
+            }}
+          />
+        }
+      />
+      <div className="w-ful p-5 bg-white border-[1px] border-(--border-color) rounded-[10px]">
+        <div className="w-full overflow-x-auto">
+          <Table />
+        </div>
+      </div>
+
     </BaseLayout>
   )
 }
 
-export default Transaksi
+export default Invoice
